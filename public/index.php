6a46feadf24d792446e973ef2f77da8220f4d8e9
aishah-akinsoji <akinsojiaishah1@gmail.com>
2025-10-29 12:27:15 +0000

    new commit

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (isset($_GET['logout']) && $_GET['logout'] == 1) {
-    require_once __DIR__ . '/src/functions.php';
+    require_once __DIR__ . '/../src/functions.php';
     $toast = [
         'display' => true,
         'message' => 'Logged out successfully',
